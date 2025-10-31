d8f56e2443693d8836c4d62774e00ec492af257f
Jonathan-py-Coder <wellies.puddles@gmail.com>
2025-10-31 18:56:12 +0000

    Add files via upload

---
+++ GrowingMyCircle.py
@@ -0,0 +1,50 @@
+import pygame
+
+pygame.init()
+
+WIDTH=600
+HIGHT=600
+
+screen=pygame.display.set_mode((WIDTH,HIGHT))
+pygame.display.set_caption("Grow My Circle")
+
+gameloop=True
+
+class Circle():
+    def __init__(self,colour,pos,radius,width):
+        self.cc=colour
+        self.cp=pos
+        self.cr=radius
+        self.cw=width
+        self.cs=screen
+
+    def draw(self):
+        self.Draw_Circle=pygame.draw.circle(self.cs,self.cc,self.cp,self.cr,self.cw)
+            
+    def grow(self,r):
+        self.cr=self.cr+r
+        self.Draw_Circle=pygame.draw.circle(self.cs,self.cc,self.cp,self.cr,self.cw)
+
+c=Circle("blue",(300,300),25,0)
+
+
+
+while gameloop:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameloop=False
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            screen.fill("black")
+            c.draw()
+            pygame.display.update()
+        
+        elif event.type == pygame.MOUSEBUTTONUP:
+            screen.fill("black")
+            c.grow(20)
+            pygame.display.update()
+
+
+
+
+pygame.quit()
